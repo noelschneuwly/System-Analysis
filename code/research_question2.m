5bc075b379449786e0bfced388b09bce97217cdf
Noel Schneuwly <noel.schneuwly@bluewin.ch>
2025-11-07 08:40:51 +0100

    Adapt Display of Plots

---
+++ code/research_question2.m
@@ -0,0 +1,103 @@
+% Final Assignment
+
+% Research Question 2
+
+
+% --- W0 parameter visualization ---
+k2vec = [5, 10, 15, 25];
+figure(1);
+hold on
+legendEntriesWater = {};
+
+figure(2);
+hold on
+legendEntriesPlant = {};
+
+
+blueBase  = [0, 0, 1];     % pure blue
+blueLight = [0.4, 0.8, 1]; % light blue
+greenBase = [0, 0.6, 0];   % pure green
+greenLight = [0.5, 1, 0.5]; % light green
+
+for j=1:4
+    % system parameters
+    PPT = 0.8; % find appropriate values
+    P = 5; % initial value
+    e = 10;
+    k1 = 3;
+    k2 = k2vec(j);
+    W0 = 0.2;
+    W = 0.1; % initial value
+    cmax = 0.5;
+    d = 0.1;
+    b = 0.15;
+    rw = 0.1; % find appropria9te value 
+
+    % --- control parameters ---
+    startTime = 0;
+    endTime   = 500;
+    timeStep  = 0.02;
+    
+    % --- compute iteration count ---
+    iterations = floor((endTime - startTime)/timeStep) + 1;
+    
+    % --- initialize vectors ---
+    timeV   = zeros(iterations, 1);
+    waterV  = zeros(iterations, 1);
+    plantV  = zeros(iterations, 1);
+    
+    % --- initialize state ---
+    stateW = W;
+    stateP = P;
+    
+    % --- simulation loop ---
+    for i = 1:iterations
+        t = startTime + (i-1)*timeStep;
+    
+        dWdt = PPT * ((stateP + k2 * W0) / (stateP + k2)) ...
+             - ((cmax * stateW) / (stateW + k1)) * stateP ...
+             - (rw * stateW);
+    
+        dPdt = e * (cmax * (stateW / (stateW + k1))) * stateP ...
+             - (d + b) * stateP;
+    
+        stateW = stateW + dWdt * timeStep;
+        stateP = stateP + dPdt * timeStep;
+    
+        % store
+        timeV(i)  = t;
+        waterV(i) = stateW;
+        plantV(i) = stateP;
+    end
+    
+    
+    % --- plots ---
+    t = (j - 1) / (length(k2vec) - 1);
+    waterColor = blueLight * (1 - t) + blueBase * t;
+    plantColor = greenLight * (1 - t) + greenBase * t;
+
+    % --- plots ---
+    figure(1)
+    plot(timeV, waterV, 'Color', waterColor, 'LineWidth', 2)
+    legendEntriesWater{end+1} = "Water (K2=" + k2vec(j) + ")";
+
+    figure(2)
+    plot(timeV, plantV, 'Color', plantColor, 'LineWidth', 2)
+    legendEntriesPlant{end+1} = "Plant (K2=" + k2vec(j) + ")";
+
+
+end 
+
+figure(1)
+xlabel("Time [days]")
+ylabel("Soil Water content [mm]")
+title("Water Dynamics for different K2 values")
+legend(legendEntriesWater)
+
+figure(2)
+xlabel("Time [days]")
+ylabel("Plant Biomass [g/m^2]")
+title("Vegetation Dynamics for different K2 values")
+legend(legendEntriesPlant)
+
+    
